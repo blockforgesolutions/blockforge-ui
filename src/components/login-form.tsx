f0bea92b04c6120cad8247ae744cb811f007c56c
Yesarib <yesaribaris23@gmail.com>
2025-03-25 14:50:54 +0300

    feat: add login page

---
+++ src/components/login-form.tsx
@@ -0,0 +1,107 @@
+'use client'
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import React, { useState } from "react"
+import { AuthResponse } from "@/types/auth"
+import { signIn } from "@/services/auth"
+import { useRouter } from "next/navigation"
+import { Spinner } from "./spinner"
+import { Alert } from "./alert"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      setLoading(true);
+      const data: AuthResponse = await signIn(email, password);
+      if (!data) {
+        setError("Login failed");
+        return;
+      }
+      setLoading(false);
+      localStorage.setItem("access_token", data.access_token);
+      router.push("/home");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      setError(error.response?.data?.message || "Login failed");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      {error && <Alert message={error} onClose={() => setError('')} type="error" />}
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required onChange={(e) => setPassword(e.target.value)} />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  {loading && <Spinner size={"small"} className="text-black" />} Login
+                </Button>
+                <Button variant="outline" className="w-full">
+                  {loading && <Spinner size={"small"} className="text-black" />} Login with Google
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="#" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
