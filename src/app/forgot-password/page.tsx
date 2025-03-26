e6bcdf63bb9ab4fbe79bc50c91d246df0649c936
Hande Budak <bdkhande@gmail.com>
2025-03-26 15:45:18 +0300

    add forgot-password page

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,9 @@
+import { ForgotPassword } from "@/components/forgot-password";
+
+export default function ForgotPasswordPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <ForgotPassword />
+    </div>
+  );
+}
