3a20abccb0b119efa94d65140c26b51ec7788c28
Yesarib <yesaribaris23@gmail.com>
2025-04-07 11:29:07 +0300

    feat: add enrollment type and apis

---
+++ src/types/enrollment.ts
@@ -0,0 +1,11 @@
+
+export type Enrollment = {
+    id:string
+    userId:string
+    courseId:string
+    paymentStatus:string
+    amount:number
+    transaction:string
+    createdAt:Date
+    updatedAt:Date
+}
