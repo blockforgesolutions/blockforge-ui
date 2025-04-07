43855ad3fbe7114774ddec489b8d9519a9fc2221
Yesarib <yesaribaris23@gmail.com>
2025-04-07 17:10:14 +0300

    feat: add get courses by category api

--- src/services/course.ts
+++ src/services/course.ts
@@ -1,5 +1,6 @@
 import { api } from "@/lib/api"
 import { Course } from "@/types/course";
+import qs from 'qs';
 
 export async function getCourses(): Promise<Course[]> {
     try {
@@ -29,4 +30,20 @@ export async function getCourseBySlug(slug: string):Promise<Course> {
     } catch (error) {
         throw error
     }
+}
+
+export async function getCoursesByCategories(categories:string[]):Promise<Course[]> {
+    try {
+        const response = await api.get('course/categories/filter', {
+            params: {
+                categories
+            },
+            paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' })
+        });
+
+        return response.data
+    } catch (error) {
+        throw error
+    }
+    
 }
