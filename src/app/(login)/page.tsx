422ab32b120752f0a53d56ee62f5503904e4e2d2
Yesarib <yesaribaris23@gmail.com>
2025-04-08 15:01:36 +0300

    refactor: replace routing to inside useEffect

--- src/app/(login)/page.tsx
+++ src/app/(login)/page.tsx
@@ -2,13 +2,18 @@
 
 import { LoginForm } from "@/components/login-form"
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
-  const token = localStorage.getItem('access_token');
   const router = useRouter();
-  if (token) {
-    router.push('/dashboard');
-  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      router.push('/dashboard');
+    }
+  }, [router]);
+  
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
