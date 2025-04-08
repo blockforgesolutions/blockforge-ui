d1d32b90c2c6d9a2d978b4d1ec73a24b5ff04bb2
Yesarib <yesaribaris23@gmail.com>
2025-04-08 15:02:39 +0300

    refactor: a element to next Link

--- src/components/verification-email.tsx
+++ src/components/verification-email.tsx
@@ -9,6 +9,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface VerificationEmailProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
@@ -40,9 +41,9 @@ export function VerificationEmail({
           </div>
           <div className="mt-4 text-center text-sm">
               Would you like to log in again? Log In{" "}
-            <a href="/" className="underline underline-offset-4">
+            <Link href="/" className="underline underline-offset-4">
               Login
-            </a>
+            </Link>
           </div>
         </CardContent>
       </Card>
