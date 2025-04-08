37aa53110eaf5854651ab28f27979c5dc2f3526b
Yesarib <yesaribaris23@gmail.com>
2025-04-08 15:00:36 +0300

    feat: add signup and upload profile picture api

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,5 @@
 import { api } from "@/lib/api"
+import { AuthResponse } from "@/types/auth";
 
 
 export async function signIn(email: string, password: string) {
@@ -11,5 +12,36 @@ export async function signIn(email: string, password: string) {
         return response.data;
     } catch (error) {
         throw error
+    }
+}
+
+type SignUp = {
+    name: string,
+    email: string,
+    password: string,
+    surname: string
+}
+
+export async function signup(formData: SignUp): Promise<AuthResponse> {
+    try {
+        const response = await api.post('/auth/signup', formData);
+
+        return response.data;
+    } catch (error) {
+        throw error
+    }
+}
+
+export async function uploadProfilePicture(formData: FormData): Promise<string> {
+    try {
+        const response = await api.post('auth/picture', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+
+        return response.data;
+    } catch (error) {
+        throw error;
     }
 }
